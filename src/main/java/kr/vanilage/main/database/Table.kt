381671916ae0614721f0a52cff355ab06932d422
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-03-13 20:05:27 +0900

    Update

--- src/main/java/kr/vanilage/main/database/Table.kt
+++ src/main/java/kr/vanilage/main/database/Table.kt
@@ -1,6 +1,6 @@
 package kr.vanilage.main.database
 
-class Table(vararg columnTypes : Any) {
+class Table(vararg columns : Any) {
     val rows = ArrayList<Row>()
     val columns = ArrayList<Any>()
 }
