b40b0e4b6ed00b56914a53b6dcfec51443e81bec
MAKEREKAM <kimyounghyun123456@gmail.com>
2024-03-13 21:59:53 +0900

    Update

--- src/main/java/kr/vanilage/main/database/Table.kt
+++ src/main/java/kr/vanilage/main/database/Table.kt
@@ -3,4 +3,8 @@ package kr.vanilage.main.database
 class Table(vararg column : Any) {
     val rows = ArrayList<Row>()
     val columns = column.clone()
+
+    fun addRow(row : Row) {
+        rows.add(row)
+    }
 }
